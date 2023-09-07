d0986d91e9c5c5b5d7b5a2a2640ce7d18507f563
felixdomingos1 <felixsdomingos93@gmail.com>
2023-09-07 16:34:23 +0100

    n0w we Deleted, Update and read all the P0sts

--- src/routers/comment/delete.ts
+++ src/routers/comment/delete.ts
@@ -1,30 +1,30 @@
-// import { Router, Request, Response, NextFunction } from "express";
-// import Comment from '../../models/comment'
-// import Post from '../../models/post'
+import { Router, Request, Response, NextFunction } from "express";
+import Comment from '../../models/comment'
+import Post from '../../models/post'
 
-// const router = Router()
+const router = Router()
 
-// router.post('/api/:postId/delete/comment/:commentId', async (req: Request, res: Response, next: NextFunction) => {
-//     const { postId, commentId } = req.params;
+router.post('/api/:postId/delete/comment/:commentId', async (req: Request, res: Response, next: NextFunction) => {
+    const { postId, commentId } = req.params;
 
-//     if(!postId || !commentId) throw new Error('commentId is required!');
+    if(!postId || !commentId) throw new Error('commentId is required!');
 
-//     try {
-//         await Comment.findOneAndRemove({ _id: commentId })
-//     } catch(err) {
-//         const error = new Error('cannot delete comment!') as CustomError 
-//         error.status = 500
-//         next(error)
-//     }
+    try {
+        await Comment.findOneAndRemove({ _id: commentId })
+    } catch(err) {
+        const error = new Error('cannot delete comment!') as CustomError 
+        error.status = 500
+        next(error)
+    }
 
-//     const updatedPost = await Post.findOneAndUpdate(
-//         {_id: postId}, 
-//         { $pull: { comments: commentId } }, 
-//         { new: true }
-//     )
+    const updatedPost = await Post.findOneAndUpdate(
+        {_id: postId}, 
+        { $pull: { comments: commentId } }, 
+        { new: true }
+    )
 
-//     res.status(201).send(updatedPost)
+    res.status(201).send(updatedPost)
 
-// })
+})
 
-// export { router as deleteCommentRouter }
+export { router as deleteCommentRouter }
