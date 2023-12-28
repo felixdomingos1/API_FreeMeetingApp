328e19f11f66487ff0cf7d2ba223a5df29f856a5
Félix Domingos <97100392+felixdomingos1@users.noreply.github.com>
2023-12-28 12:21:32 +0100

    Update update.ts
    Substituí IPostDocument pelo tipo real de documento que você está usando para os Posts, conforme definido em seu modelo Post. Além disso, certifiquei-me de ter as definições de tipo corretas para o modelo Post e ajuste-as conforme necessário no código.

--- src/routers/post/update.ts
+++ src/routers/post/update.ts
@@ -1,30 +1,41 @@
 import { Router, Request, Response, NextFunction } from "express";
-import Post from '../../models/post'
+import Post, { IPostDocument } from '../../models/post';
 
-const router = Router()
+interface CustomError extends Error {
+    status?: number;
+}
+
+const router = Router();
 
 router.put('/api/update/post/:id', async (req: Request, res: Response, next: NextFunction) => {
     const { title, content } = req.body;
-    const { id } = req.params
+    const { id } = req.params;
 
-    if(!title || !content || !id) {
-        const error = new Error('title, content and id are required') as CustomError 
-        error.status = 400
-        next(error)
-    }   
+    if (!title || !content || !id) {
+        const error = new Error('title, content, and id are required') as CustomError;
+        error.status = 400;
+        return next(error);
+    }
 
-    const newPost = await Post.findOneAndUpdate(
-        { _id: id }, 
-        {$set: { 
-            title, 
-            content 
-        }}, { 
-            new: true 
+    try {
+        const newPost: IPostDocument | null = await Post.findOneAndUpdate(
+            { _id: id },
+            { $set: { title, content } },
+            { new: true }
+        );
+
+        if (!newPost) {
+            const error = new Error('Post not found') as CustomError;
+            error.status = 404;
+            return next(error);
         }
-    )
 
-    res.status(201).send(newPost)
+        res.status(201).send(newPost);
+    } catch (error) {
+        const customError = error as CustomError;
+        customError.status = 500;
+        return next(customError);
+    }
+});
 
-})
-
-export { router as updatePostRouter }
+export { router as updatePostRouter };
